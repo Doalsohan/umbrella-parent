e13f68f2ac8df3b73911c847acaf36329fc52229
yaxi.hu <hyx199170@163.com>
2023-09-12 20:39:30 +0800

    demo 应用配置优先级示例

--- umbrella-chapter/umbrella-boot/umbrella-demo/src/main/java/com/umbrella/demo/App.java
+++ umbrella-chapter/umbrella-boot/umbrella-demo/src/main/java/com/umbrella/demo/App.java
@@ -1,7 +1,12 @@
 package com.umbrella.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 /**
  * Hello world!
@@ -11,5 +16,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class App {
     public static void main( String[] args ) {
         SpringApplication.run(App.class,args);
+    }
+
+
+    @Slf4j
+    @Component
+    static class ConfigPriorityRunner implements ApplicationRunner {
+        @Value("${name}")
+        private String name;
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+            log.info(name);
+        }
     }
 }
--- umbrella-chapter/umbrella-boot/umbrella-demo/src/main/java/com/umbrella/demo/service/impl/EnvironmentServiceImpl.java
+++ umbrella-chapter/umbrella-boot/umbrella-demo/src/main/java/com/umbrella/demo/service/impl/EnvironmentServiceImpl.java
@@ -3,16 +3,16 @@ package com.umbrella.demo.service.impl;
 import com.umbrella.demo.entity.Environment;
 import com.umbrella.demo.repository.EnvironmentRepository;
 import com.umbrella.demo.service.EnvironmentService;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
 @Slf4j
 @Service
+@AllArgsConstructor
 public class EnvironmentServiceImpl implements EnvironmentService {
-    @Resource
-    private EnvironmentRepository environmentRepository;
+
+    private final EnvironmentRepository environmentRepository;
 
     @Override
     public Environment recordEnvironment(Environment environment) {
