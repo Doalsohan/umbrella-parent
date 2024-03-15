cca403c414e246c1b51f79739b39da7420299dd7
yaxi.hu <hyx199170@163.com>
2024-03-15 19:29:40 +0800

    第三方同步数据接口签名验签

--- umbrella-chapter/umbrella-boot/umbrella-demo/src/main/java/com/umbrella/demo/sdk/java/EmojiUtil.java
+++ umbrella-chapter/umbrella-boot/umbrella-demo/src/main/java/com/umbrella/demo/sdk/java/EmojiUtil.java
@@ -1,5 +1,9 @@
 package com.umbrella.demo.sdk.java;
 
+import cn.hutool.core.util.HexUtil;
+
+import java.math.BigInteger;
+
 public class EmojiUtil {
 
     public static void main(String[] args) {
@@ -8,6 +12,10 @@ public class EmojiUtil {
         System.out.println(JvmHello.selfInfo);
 
         System.out.println(JvmHello.a);
+
+        BigInteger bigInteger = new BigInteger("3d0490458ee3118ead7c2a90702cb574209442c5",16);
+        BigInteger mod = bigInteger.mod(new BigInteger("4"));
+        System.out.println(mod.intValue());
     }
 
 }
